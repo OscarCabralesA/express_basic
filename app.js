ae57f96490a6a66f6f523b8c29ca6e9f57216ccd
OscarCabralesA <aran.danonoconuva@gmail.com>
2022-04-25 21:33:30 -0500

    Added a path that receives params

--- app.js
+++ app.js
@@ -2,20 +2,32 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
+// initializing the app
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
 
+// initial path in localhost:3000
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+
+// returning simple text
 app.get('/launchx', (req, res) => {
     res.send('Bienvenidos a launchx')
 })
 
+// returns an object 
+// http://localhost:3000/explorersInNode
 app.get('/explorersInNode', (req, res) => {
     const explorer = {name: "Mauri", msg: "Helloo"}
     res.send(explorer)
 })
 
+// to receive params from url 
+// http://localhost:3000/explorers/mauri
+// req.params = {"explorerName":"mauri"}
+app.get('/explorers/:explorerName', (req, res) => {
+    res.send(req.params)
+})
